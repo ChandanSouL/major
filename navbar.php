85ea5f90f7e6d467ef2a46a89db401d3a871f84d
Chandan Prajwal <76419264+ChandanSouL@users.noreply.github.com>
2023-02-13 13:42:49 +0000

    navbar login-signup

---
+++ navbar.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Navigation Bar</title>
+    <style>
+      /* Add styling for the navigation bar */
+      .navbar {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        padding: 10px 20px;
+        background-color: #333;
+        color: #fff;
+      }
+
+      .navbar a {
+        color: #fff;
+        text-decoration: none;
+        margin-right: 20px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="navbar">
+      <a href="login.html">Login</a>
+      <a href="signup.html">Sign Up</a>
+    </div>
+  </body>
+</html>
